576cf6bd910f3062d68a433c811c832ff735293d
Jonathan Pereira <jonathan14willian@icloud.com>
2023-07-23 17:16:08 -0300

    Inclusão do To do List

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ Inspencionar no navegador
 console.log('Hello World');
 */
 
+// Criando um contador
+
+
+
+const numContador = document.querySelector('#num-contador');
+const btnMais = document.querySelector('#btn-mais');
+const btnMenos = document.querySelector('#btn-menos');
+let contador = 0;
+
+btnMais.addEventListener('click', function(){
+    numContador.innerHTML = contador += 1;
+} );
+
+btnMenos.addEventListener('click', function(){
+    numContador.innerHTML = contador -= 1;
+} );
